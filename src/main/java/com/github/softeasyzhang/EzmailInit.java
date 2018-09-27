285af53fd96e3471164a8c0a7fd9670fbd650a94
zhangyi02 <zhangyi02@daojia-inc.com>
2018-09-27 10:50:14 +0800

    base package

---
+++ src/main/java/com/github/softeasyzhang/EzmailInit.java
@@ -0,0 +1,14 @@
+package com.github.softeasyzhang;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author EasyZhang
+ * @date 2018/9/27 -  10:36
+ */
+
+public class EzmailInit {
+
+    private static final Logger logger = LoggerFactory.getLogger(EzmailInit.class);
+}
